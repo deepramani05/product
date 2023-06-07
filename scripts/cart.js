d5a303a808be647de5bb7f50c69c33e76ed463c7
deepramani05 <deepramani4653@gmail.com>
2023-06-08 00:22:25 +0530

    total cart price

--- scripts/cart.js
+++ scripts/cart.js
@@ -2,3 +2,12 @@ import nav from "../component/nav.js";
 
 
 document.getElementById("nav").innerHTML=nav()
+
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let totalprice = 0;
+for (let i = 0; i < cart.length; i++) {
+  totalprice += cart[i].price * cart[i].qty;
+}
+
+document.getElementById("cart").innerHTML = `Total Price: ${totalprice}`;
