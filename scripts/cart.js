9e7879d00a6fc14f5e40b16756803a7804845217
deepramani05 <deepramani4653@gmail.com>
2023-06-08 14:53:27 +0530

    cart age

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,13 +1,124 @@
-import nav from "../component/nav.js";
-
-
-document.getElementById("nav").innerHTML=nav()
 
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
-let totalprice = 0;
-for (let i = 0; i < cart.length; i++) {
-  totalprice += cart[i].price * cart[i].qty;
-}
 
-document.getElementById("cart").innerHTML = `Total Price: ${totalprice}`;
+let product = (data) => {
+  let price = 0
+  for (let i=0;i<cart.length;i++){
+    price += cart[i].price * cart[i].qty * 10 
+  }
+
+  document.getElementById("cart").innerHTML= `Total : -  $ ${price}`
+
+  data.map((ele) => {
+
+    let main = document.createElement("div")
+    main.setAttribute("id","main")
+
+
+
+
+      let dmain = document.createElement("div")
+      dmain.setAttribute("id","data")
+
+
+
+          let idiv = document.createElement("div")
+          idiv.setAttribute("id","image-div")
+
+
+              let imag = document.createElement("div")
+              imag.setAttribute("id","img")
+
+                  let img = document.createElement("img")
+                  img.src=ele.images[1]
+
+              imag.append(img)
+
+
+              let name = document.createElement("div")
+              name.setAttribute("id","name")
+
+                  let title = document.createElement("h1")
+                  title.innerHTML=ele.title
+
+              name.append(title)
+
+
+          idiv.append(imag,name)
+
+
+
+
+
+          let pdiv = document.createElement("div")
+          pdiv.setAttribute("id","price-div")
+
+
+
+              let qdiv = document.createElement("div")
+              qdiv.setAttribute("id","qty")
+              
+                  let sdiv1 = document.createElement("div")
+
+                      let a1 = document.createElement("a")
+                      a1.innerHTML="-"
+
+                      
+
+                  sdiv1.append(a1)
+
+
+                  let sdiv2 = document.createElement("div")
+
+                      let a2 = document.createElement("a")
+                      a2.innerHTML=ele.qty
+
+                  sdiv2.append(a2)
+
+
+                  let sdiv3 = document.createElement("div")
+
+                      let a3 = document.createElement("a")
+                      a3.innerHTML="+"
+
+                      a3.addEventListener("click", () => {
+                  
+                        cart.map((item, pro) => {
+                            cart[pro].qty -= 1;
+                        });
+
+                      });
+
+                  sdiv3.append(a3)
+
+              qdiv.append(sdiv1,sdiv2,sdiv3)
+
+          pdiv.append(qdiv)
+
+
+
+
+          let pmain = document.createElement("div")
+          pmain.setAttribute("id","price")
+
+              let price = document.createElement("h1")
+              price.innerHTML=ele.price * 10
+              
+          pmain.append(price)
+
+
+        dmain.append(idiv,pdiv,pmain)
+
+
+      main.append(dmain)
+
+
+
+    document.getElementById("product").append(main)
+
+  })
+} 
+
+product(cart)
+
--- scripts/product.js
+++ scripts/product.js
@@ -1,59 +1,59 @@
 
 let output = (product) => {
-    product.map((ele) => {
+  product.map((ele) => {
 
-        let div = document.createElement("div")
+    let div = document.createElement("div")
 
-        let img = document.createElement("img")
-        img.src=ele.images[1]
+    let img = document.createElement("img")
+    img.src = ele.images[1]
 
-        let title = document.createElement("h1")
-        title.innerHTML=ele.title
+    let title = document.createElement("h1")
+    title.innerHTML = ele.title
 
-        let price = document.createElement("h2")
-        price.innerHTML=`Price = ${ele.price*5}`
+    let price = document.createElement("h2")
+    price.innerHTML = `Price = ${ele.price * 10}`
 
-        let rating = document.createElement("h3")
-        rating.innerHTML=`Rating = ${ele.rating}`
+    let rating = document.createElement("h3")
+    rating.innerHTML = `Rating = ${ele.rating}`
 
-        let discount = document.createElement("h3")
-        discount.innerHTML=`Discount = ${ele.discountPercentage}`
+    let discount = document.createElement("h3")
+    discount.innerHTML = `Discount = ${ele.discountPercentage}`
 
-        let btn = document.createElement("button")
-        btn.innerHTML="Add To Cart"
+    let btn = document.createElement("button")
+    btn.innerHTML = "Add To Cart"
 
-        let btn2 = document.createElement("button")
-        btn2.innerHTML="Buy Now"
+    let btn2 = document.createElement("button")
+    btn2.innerHTML = "Buy Now"
 
-        div.append(img,title,price,rating,discount,btn,btn2)
+    div.append(img, title, price, rating, discount, btn, btn2)
 
-        document.getElementById("product").append(div)
+    document.getElementById("product").append(div)
 
-        btn.addEventListener("click", () => {
-            let cart = JSON.parse(localStorage.getItem("cart")) || [];
-            let exists = false;
-            // checking for existing cart
-            cart.map((item, pro) => {
-              if (item.id == ele.id) {
-                cart[pro].qty += 1;
-                exists = true;
-              }
-            });
-        
-            if (!exists) {
-              cart.push({ ...ele, qty: 1 });
-        
-              alert("added to cart");
-            }
-        
-            localStorage.setItem("cart", JSON.stringify(cart));
-          });
+    btn.addEventListener("click", () => {
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+      
+      let exists = false;
 
-    })
+      cart.map((item, pro) => {
+        if (item.id == ele.id) {
+          cart[pro].qty += 1;
+          exists = true;
+        }
+      });
+
+      if (!exists) {
+        cart.push({ ...ele, qty: 1 });
+
+        alert("added to cart");
+      }
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+    });
+  })
 }
 
 
-fetch ("https://dummyjson.com/products")
-.then((value) => value.json())
-.then((value) => output(value.products))
+fetch("https://dummyjson.com/products")
+  .then((value) => value.json())
+  .then((value) => output(value.products))
 // .then((value) => console.log(value.products))
